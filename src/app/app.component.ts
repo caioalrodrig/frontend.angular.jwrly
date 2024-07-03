c597ee379ac5f622bee02d0ea2b0b5e9397c4cc9
caioalrodrig <caio.almeida@estudante.ufjf.br>
2024-07-03 12:29:17 -0300

    feat: module structures and angular materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Directive, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, MatButtonModule, MatSelectModule],
+  exportAs: "backend",
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent{
   title = 'jewelAPI';
+  nomePortal = 'Caio\'s';
+
+  getForm(submiText: HTMLInputElement){
+    console.log(submiText.value);
+  }
 }
